8519182346c454ada20c05f8592902b0248aa380
kevyn <catrix.kevyn@gmail.com>
2023-06-28 14:27:50 +0200

    push

--- arachnida/spider.py
+++ arachnida/spider.py
@@ -1,8 +1,23 @@
 import sys
+import os
+import pprint
+import time
+import urllib.error
+import urllib.request
 
-def afficher_arguments():
-    arguments = sys.argv[1:]  # Ignorer le premier argument (nom du script)
-    for arg in arguments:
-        print(arg)
+def download_file(url, dst_path):
+    try:
+        with urllib.request.urlopen(url) as web_file:
+            data = web_file.read()
+            with open(dst_path, mode='wb') as local_file:
+                local_file.write(data)
+    except urllib.error.URLError as e:
+        print(e)
 
-afficher_arguments()
+def take_img(url, path):
+    i = 0
+    type_img = ["jpg", "png", "gif", "jpeg", "bmp"]
+    download_file(url, path + "img " + str(i))
+    
+
+take_img(sys.argv[1], "img/")
